b27039ca79b01d76bbaceca2ff7b182a93226757
Christoph Pickl <cpickl@surfer-172-30-17-114-hotspot.internet-for-guests.com>
2017-02-21 16:31:27 +0100

    kodein done

--- src/main/kotlin/com/github/christophpickl/awesomekotlin/kodein.kt
+++ src/main/kotlin/com/github/christophpickl/awesomekotlin/kodein.kt
@@ -2,20 +2,59 @@ package com.github.christophpickl.awesomekotlin
 
 import com.github.salomonbrys.kodein.*
 
+// KOtlin DEpendency INjection
+// kotlin like, small DI container: https://github.com/SalomonBrys/Kodein
+// https://salomonbrys.github.io/Kodein/
+
+/*
+more:
+- Aware interfaces remove syntax to reference kodein
+- Configurable Kodein via maven dep "kodein-conf" to have something like "mutable kodein config object"
+- god complex: kodein.global instance ;)
+- type erasure problem bypassed by genericToken(), but performance problem! use erased* methods instead
+    - use "kodein-erased" module instead to default to erased version (faster but unsafe)
+- scopes are supported
+
+compile 'com.github.salomonbrys.kodein:kodein:3.2.0'
+compile 'com.github.salomonbrys.kodein:kodein-android:3.2.0'
+ */
 fun main(args: Array<String>) {
     val kodein = configureKodein()
+    println(kodein.container.bindings.description)
 
-    Resource1(kodein).printGreet()
-    Resource2(kodein).printGreet()
+//    Resource1(kodein).printGreet()
+    Resource1(kodein).printPersonalGreet("Christoph")
+//    Resource2(kodein).printGreet()
+
+    println("Building subKodein ...")
+    val subKodein = Kodein {
+        extend(kodein)
+        // useful for tests ;) need to allow modules explicitly to be able to override bindings
+        bind<Repository>(overrides = true) with singleton { ProxyRepository(overriddenInstance()) }
+
+        onReady {
+            println("subKodein onReady")
+            val repo = instance<Repository>()
+            repo.initState()
+        }
+    }
+    println(subKodein.container.bindings.description)
+    Resource1(subKodein).printGreet()
 }
 
 fun configureKodein() = Kodein {
-    // https://salomonbrys.github.io/Kodein/
     println("Configure Kodein ...")
+
     constant("lang") with Lang.DE
-    bind<Repository>() with singleton { RepositoryImpl(instance("lang")) }
+
+    import(RepoModule)
+
     bind<ProvidedService>() with provider { ProvidedServiceImpl() }
     bind<SingletonService>() with singleton { SingletonServiceWithRepo(kodein) }
+
+    bind<LazyService>() with factory { name: String -> ServiceProvidedRuntimeWithAssistedInject(name, kodein) }
+
+    bind<Logger>() with factory { cls: Class<*> -> SysoutLogger(cls) }
 
     // bind<Dice>() with factory { sides: Int -> RandomDice(sides) }
     // bind<DataSource>() with eagerSingleton { SqliteDS.open("path/to/file") }
@@ -29,14 +68,34 @@ fun configureKodein() = Kodein {
     println("Configure Kodein ... DONE")
 }
 
+interface LazyService {
+    fun greeting(): String
+}
+class ServiceProvidedRuntimeWithAssistedInject(
+        private val runtimeName: String,
+        kodein: Kodein,
+        private val repository: Repository = kodein.instance()
+) : LazyService {
+    override fun greeting() = "${javaClass.simpleName} says: ${repository.loadSalutation()} $runtimeName!"
+}
+
 class Resource1(
         kodein: Kodein,
         private val singletonService: SingletonService = kodein.instance(),
-        private val providedService: ProvidedService = kodein.instance()
+        private val providedService: ProvidedService = kodein.instance(),
+        private val lazyServiceFactory: (String) -> LazyService = kodein.factory()
 ) {
+    private val log: Logger = kodein.withClassOf(this).instance()
+
     fun printGreet() {
+        log.debug("printGreet()")
         println(singletonService.greeting())
-        println(providedService.greeting())
+//        println(providedService.greeting())
+    }
+
+    fun printPersonalGreet(name: String) {
+        log.debug("printPersonalGreet(name)")
+        println(lazyServiceFactory(name).greeting())
     }
 }
 
@@ -79,8 +138,16 @@ class ProvidedServiceImpl : ProvidedService {
 
 }
 
+val RepoModule = Kodein.Module {
+    println("Preparing RepoModule ...")
+    bind<Repository>() with singleton { RepositoryImpl(instance("lang")) }
+}
+
 interface Repository {
     fun loadSalutation(): String
+    fun initState() {
+        println("${javaClass.simpleName} initState()")
+    }
 }
 
 class RepositoryImpl(private val lang: Lang) : Repository {
@@ -92,4 +159,25 @@ class RepositoryImpl(private val lang: Lang) : Repository {
 }
 enum class Lang {
     DE, EN;
+}
+
+class ProxyRepository(private val originalRepo: Repository) : Repository {
+    override fun loadSalutation(): String {
+        println("ProxyRepository => Do something more in salutation")
+        return originalRepo.loadSalutation()
+    }
+
+}
+
+interface Logger {
+    fun debug(message: String)
+}
+class SysoutLogger(cls: Class<*>) : Logger {
+
+    private val className = cls.simpleName
+
+    override fun debug(message: String) {
+        println("$className [DEBUG] $message")
+    }
+
 }
